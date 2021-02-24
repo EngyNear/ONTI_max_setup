558f906fef94afb1bd95bf9afabfad33bf3e5534
engynear <32967577+EngyNear@users.noreply.github.com>
2021-02-25 00:36:06 +0400

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,5 +2,11 @@
 BASEDIR=$(dirname "$0")
 
 sudo systemctl disable pioneer-bricks-wlan
-sudo systemctl stop pioneer-bricks-wlan
+sudo systemctl enable wlan-butterfly
+sudo systemctl enable code-server-wlan
+sudo systemctl enable web-wlan-menu
+
+# sudo systemctl enable code-server-eth
+# sudo systemctl enable eth-butterfly
+# sudo systemctl enable web-eth-menu
 python3 $BASEDIR/../src/edit_webmenu.py
