39096765706f2ed4ab77fe04446d109d67417082
engynear <32967577+EngyNear@users.noreply.github.com>
2021-03-10 08:31:23 +0300

    hf v3

--- bash/COPTER_SETUP.sh
+++ bash/COPTER_SETUP.sh
@@ -5,7 +5,7 @@ python3 $BASEDIR/../src/usb2s.py
 python3 $BASEDIR/../src/camera2wlan.py
 
 sudo nmcli device wifi connect "OlyNTI" password "mospolytech1" name "ONTI"
-sudo nmcli con up "OlyNTI"
+sudo nmcli con up "ONTI"
 
 sh $BASEDIR/start_wlan_services.sh
 python3 $BASEDIR/../../geoscan_ws/src/gs_core/src/restart.py
--- bash/STAND_SETUP.sh
+++ bash/STAND_SETUP.sh
@@ -4,6 +4,6 @@ python3 $BASEDIR/../src/s2usb.py
 python3 $BASEDIR/../src/camera2eth.py
 
 # sudo nmcli device wifi connect "OlyNTI" password "mospolytech1" name "ONTI"
-sudo nmcli con down "OlyNTI"
+sudo nmcli con down "ONTI"
 
 sh $BASEDIR/start_eth_services.sh
