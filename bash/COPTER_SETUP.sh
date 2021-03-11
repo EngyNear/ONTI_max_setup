eb9b2fb7f1d4669056233fdf13de54c4dc56b7c7
engynear <32967577+EngyNear@users.noreply.github.com>
2021-03-11 12:52:08 +0300

    Update COPTER_SETUP.sh

--- bash/COPTER_SETUP.sh
+++ bash/COPTER_SETUP.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname "$0")
 python3 $BASEDIR/../src/usb2s.py
 python3 $BASEDIR/../src/camera2wlan.py
 
-sudo nmcli device wifi connect "OlyNTI" password "mospolytech1" name "ONTI"
-sudo nmcli con up "ONTI"
+#sudo nmcli device wifi connect "OlyNTI" password "mospolytech1" name "ONTI"
+#sudo nmcli con up "ONTI"
 
-sh $BASEDIR/start_wlan_services.sh
+#sh $BASEDIR/start_wlan_services.sh
 python3 $BASEDIR/../../geoscan_ws/src/gs_core/src/restart.py
